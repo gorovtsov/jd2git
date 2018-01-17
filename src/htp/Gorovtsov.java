ca6dbba9c9b368409ed0fca247f6c1e9fcf84c0d
Denis <Buridza0@gmail.com>
2018-01-17 14:17:18 +0300

    added new field

--- src/htp/Gorovtsov.java
+++ src/htp/Gorovtsov.java
@@ -1,6 +1,7 @@
 package htp;
 
 public class Gorovtsov {
+    private String newField;
     public static void main(String[] args) {
         System.out.println("First str changed by gorovtsov");
         System.out.println("First str changed by Denis");
