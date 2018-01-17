059081c8443613b026d526da7fe4c86188d8e686
Denis <Buridza0@gmail.com>
2018-01-17 14:12:18 +0300

    changed sout

--- src/htp/Gorovtsov.java
+++ src/htp/Gorovtsov.java
@@ -2,6 +2,7 @@ package htp;
 
 public class Gorovtsov {
     public static void main(String[] args) {
-        System.out.println("First str");
+        System.out.println("First str changed by Denis");
+
     }
 }
