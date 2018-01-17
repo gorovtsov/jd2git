1e49dc10f38fa0bd391e10ac452594e76fc6a8b5
Alexander Gorovtsov <garavek94@gmail.com>
2018-01-17 14:12:42 +0300

    First sout changed

--- src/htp/Gorovtsov.java
+++ src/htp/Gorovtsov.java
@@ -2,6 +2,6 @@ package htp;
 
 public class Gorovtsov {
     public static void main(String[] args) {
-        System.out.println("First str");
+        System.out.println("First str changed by gorovtsov");
     }
 }
